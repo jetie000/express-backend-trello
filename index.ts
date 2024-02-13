0896b1b3f72c8f5f453a937d663482c58289e3f7
jetie000 <makeenko.005@gmail.com>
2024-02-13 15:10:35 +0300

    feat: finish db

--- index.ts
+++ index.ts
@@ -3,7 +3,10 @@ import cors from 'cors'
 
 const app = express();
 const port = 8080;
+
 app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.listen(port, () => {
     console.log(`server listening at http://localhost:${port}`);
